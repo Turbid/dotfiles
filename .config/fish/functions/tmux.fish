668daa9f190c6713b56bf14b77740b7e562c2cd4
Ilya Demyanov <turbidit@gmail.com>
2019-01-29 22:00:36 +0300

    Fixed tmux alias for fish shell

--- .config/fish/functions/tmux.fish
+++ .config/fish/functions/tmux.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function tmux --description 'alias tmux=tmux -2 attach; or tmux -2 new'
-	command tmux -2 attach; or tmux -2 new $argv;
+	command tmux -2 attach; or command tmux -2 new $argv;
 end
