b6c1134682021f1f4713462137701071a9b8b06e
Ilya Demyanov <turbidit@gmail.com>
2022-12-19 16:15:51 +0300

    nvim: from vimscript to lua

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,25 @@
+local cmd = vim.cmd
+local opt = vim.opt
+
+opt.number = true
+--opt.cursorline = true
+
+opt.title = true
+opt.laststatus = 3
+--opt.showmode = false
+
+opt.clipboard = 'unnamedplus'
+
+opt.ignorecase = true 
+opt.smartcase = true
+opt.inccommand = 'split'
+
+opt.listchars = 'tab:▒░,trail:·,eol:¶'
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smartindent = true
